b12aab2f2bda270da6942e4f1af2b2aa6ae5dc60
dvdo <diegog367@gmail.com>
2025-03-03 22:56:01 -0800

    Use assert instead of sassert

--- third_party/SnDsL/include/SDL_assert.h
+++ third_party/SnDsL/include/SDL_assert.h
@@ -1,8 +1,4 @@
 #pragma once
-#include <nds/arm9/sassert.h>
-// #include <assert.h>
+#include <assert.h>
 
-// inline void SDL_assert(unsigned char condition) { // needed when assert is redefined
-// 	sassert(condition, "Assertion failed!");
-// }
-#define SDL_assert(condition) sassert(condition, "")
+#define SDL_assert(condition) assert(condition)
